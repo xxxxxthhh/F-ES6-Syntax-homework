19ecfb110b29845d74bf1acef2068e63635c35c3
KyleXu <kyle_x@foxmail.com>
2020-12-05 01:17:25 +0800

    [XTH]: finished two practices

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) => {
+  const {data, column} = input;
+  const output = [];
+  data.forEach((obj) => {
+    const newObj = {};
+    for (let i = 0; i < column.length; i++){
+      newObj[column[i].name] = obj[i];
+    }
+    output.push(newObj);
+  });
+  return output;
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,23 @@
-const inject
+const inject = (items, sections) =>{
+  const answer = [];
+  // const totalLength  = sections.length + items.length;
+  const orderedSection = sections.sort((obj1, obj2) => obj1.index - obj2.index);
+  let currentIndex = 0;
+  let sectionIndex = 0;
+
+  items.forEach((item) => {
+    let injection = orderedSection.findIndex(section => section.index === currentIndex);
+    if (injection !== -1){
+      answer.push(orderedSection[sectionIndex].content);
+      sectionIndex++;
+      answer.push(item);
+      currentIndex++;
+    }else {
+      answer.push(item);
+      currentIndex++;
+    }
+  } );
+
+  return answer;
+};
 export { inject };
